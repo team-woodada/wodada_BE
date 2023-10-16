e029283d7d76aa2f5863c5898a1f00b7cf103074
zeniverse <0219alice@gmail.com>
2023-10-16 23:08:32 +0900

    feat : Member 정보 조회 및 수정 기능 추가 (#10)
    유저 정보 조회 및 수정 처리 구현

--- src/main/java/com/me/wodada/member/service/MemberService.java
+++ src/main/java/com/me/wodada/member/service/MemberService.java
@@ -1,9 +1,16 @@
 package com.me.wodada.member.service;
 
+import com.me.wodada.image.AmazonS3Service;
+import com.me.wodada.member.domain.Member;
+import com.me.wodada.member.dto.request.MemberInfoUpdateReq;
+import com.me.wodada.member.dto.response.MemberInfoRes;
 import com.me.wodada.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -11,5 +18,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final AmazonS3Service amazonS3Service;
 
+    @Transactional
+    public void updateMemberInfo(Member member, MemberInfoUpdateReq memberInfoUpdateReq, List<MultipartFile> images) {
+        if (member.getProfileImageUrl() != null){
+            amazonS3Service.deleteFile(member.getProfileImageUrl());
+        }
+
+        String profileUrl = amazonS3Service.uploadFile(images).get(0);
+        updateMember(member, memberInfoUpdateReq, profileUrl);
+
+    }
+
+    private void updateMember(Member member, MemberInfoUpdateReq memberInfoUpdateReq, String profileUrl) {
+        // TODO : Exception 처리
+        Member findMember = memberRepository.findByEmailAndProvider(member.getEmail(), member.getProvider())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+        findMember.updateProfile(memberInfoUpdateReq, profileUrl);
+        // TODO : Locaiont update -> Role User로 변경
+    }
+
+    public MemberInfoRes getMemberInfo(Member member) {
+        return MemberInfoRes.from(member);
+    }
 }

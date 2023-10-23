a59fce08be98baac4482b20143a5515b8b23fd41
zeniverse <0219alice@gmail.com>
2023-10-23 21:56:55 +0900

    refactor : Member Entity 수정 (#18)
    Member Entity에 Pet과 연관 관계를 맺어준다

--- src/main/java/com/me/wodada/member/domain/Member.java
+++ src/main/java/com/me/wodada/member/domain/Member.java
@@ -1,11 +1,15 @@
 package com.me.wodada.member.domain;
 
 import com.me.wodada.member.dto.request.MemberInfoUpdateReq;
+import com.me.wodada.pet.domain.Pet;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -32,11 +36,7 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    private int age;
-
     private String ageRange;
-
-    private Boolean isAgePublic = true;
 
     private String address;
 
@@ -47,29 +47,37 @@ public class Member {
 
     private Double rating;
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Pet> petList = new ArrayList<>();
+
     @Builder
     public Member(Role role, String email, String nickname, String profileImageUrl,
-                  Gender gender, int age, String ageRange, Boolean isAgePublic, String address, String bio,
+                  Gender gender,String ageRange, String address, String bio,
                   String provider, Double rating) {
         this.role = role;
         this.email = email;
         this.nickname = nickname;
         this.profileImageUrl = profileImageUrl;
         this.gender = gender;
-        this.age = age;
         this.ageRange = ageRange;
-        this.isAgePublic = isAgePublic;
         this.address = address;
         this.bio = bio;
         this.provider = provider;
         this.rating = rating;
     }
 
-    public void updateProfile(MemberInfoUpdateReq request, String profileUrl) {
+    public void updateProfile(MemberInfoUpdateReq request) {
         this.nickname = request.getNickname();
-        this.isAgePublic = request.getIsAgePublic();
         this.address = request.getAddress();
         this.bio = request.getBio();
+        this.role = Role.USER;
+    }
+
+    public void addProfileImage(String profileUrl){
         this.profileImageUrl = profileUrl;
+    }
+
+    public void addPet(Pet pet){
+        this.petList.add(pet);
     }
 }

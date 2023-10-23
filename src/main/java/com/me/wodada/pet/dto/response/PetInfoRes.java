385479e0871c455084897db1a5c021b75707cc63
zeniverse <0219alice@gmail.com>
2023-10-23 23:39:02 +0900

    feat : PetInfoRes 생성 (#11)
    회원 정보를 조회할 때, Pet 정보를 함께 조회하기 위해서 사용하는 Response 파일

---
+++ src/main/java/com/me/wodada/pet/dto/response/PetInfoRes.java
@@ -0,0 +1,46 @@
+package com.me.wodada.pet.dto.response;
+
+import com.me.wodada.member.domain.Gender;
+import com.me.wodada.pet.domain.Pet;
+import lombok.*;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Lob;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PetInfoRes {
+    private String name;
+    private String profileImageUrl;
+    private String gender;
+    private Boolean isNeutered;
+    private Double weight;
+    private int age;
+    private String personality;
+
+    @Builder
+    public PetInfoRes(String name, String profileImageUrl, String gender,
+                      Boolean isNeutered, Double weight, int age, String personality) {
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+        this.gender = gender;
+        this.isNeutered = isNeutered;
+        this.weight = weight;
+        this.age = age;
+        this.personality = personality;
+    }
+
+    public static PetInfoRes from (Pet pet){
+        return PetInfoRes.builder()
+                .name(pet.getName())
+                .profileImageUrl(pet.getProfileImageUrl())
+                .gender(pet.getGender().toString())
+                .isNeutered(pet.getIsNeutered())
+                .weight(pet.getWeight())
+                .age(pet.getAge())
+                .personality(pet.getPersonality())
+                .build();
+    }
+}

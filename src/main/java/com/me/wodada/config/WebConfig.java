48fb88ddb1e8a7e3e673abffafea0e9dda14a4cf
zeniverse <0219alice@gmail.com>
2023-11-03 23:52:05 +0900

    feat: WebConfig 생성 (#22)
    Interceptor 설정을 위해 WebConfig 파일 생성
    - Interceptor가 적용될 URL 설정

---
+++ src/main/java/com/me/wodada/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.me.wodada.config;
+
+import com.me.wodada.common.PermissionInterceptor;
+import com.me.wodada.common.RoleInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final PermissionInterceptor permissionInterceptor;
+    private final RoleInterceptor roleInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(roleInterceptor)
+                .addPathPatterns("/api/post/**")
+                .excludePathPatterns("/api/post/get/**");
+        registry.addInterceptor(permissionInterceptor)
+                .addPathPatterns("/api/post/edit/**")
+                .addPathPatterns("/api/post/delete/**");// 인터셉터 실행 O
+    }
+
+}

1b6ce39a03d7262e21f9214a90002e9fbcc4a799
zeniverse <0219alice@gmail.com>
2023-11-06 09:53:18 +0900

    feat :  Post Enitity에 method 추가 (#22)
    기존 Post Entity에
    - 글 작성 시 PostCreate를 사용해 Post 생성하는 method 추가
    - 글 수정 시 update를 위한 method 추가

--- src/main/java/com/me/wodada/post/domain/Post.java
+++ src/main/java/com/me/wodada/post/domain/Post.java
@@ -3,6 +3,8 @@ package com.me.wodada.post.domain;
 import com.me.wodada.common.BaseEntity;
 import com.me.wodada.image.domain.PostImage;
 import com.me.wodada.member.domain.Member;
+import com.me.wodada.post.dto.request.PostCreate;
+import com.me.wodada.post.dto.request.PostEdit;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -38,17 +40,30 @@ public class Post extends BaseEntity{
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
-    private List<PostImage> postImageList = new ArrayList<>();
-
     @Builder
-    public Post(Long id, Board board, String title, String content, Member member,
-                List<PostImage> postImageList) {
+    public Post(Long id, Board board, String title, String content, Member member) {
         this.id = id;
         this.board = board;
         this.title = title;
         this.content = content;
         this.member = member;
-        this.postImageList = postImageList;
+    }
+
+    public static Post form(Member member, PostCreate postCreate){
+        return Post.builder()
+                .board(toChangeBoard(postCreate.getBoard()))
+                .title(postCreate.getTitle())
+                .content(postCreate.getContent())
+                .member(member)
+                .build();
+    }
+
+    private static Board toChangeBoard(String board) {
+        return board.equals("manager") ? Board.MANAGER : Board.MATE;
+    }
+
+    public void update(PostEdit postEdit) {
+        this.title = postEdit.getTitle();
+        this.content = postEdit.getContent();
     }
 }
